f82715fe84e6a9b9f4248899d8ffe45fc36409ae
Gustavo Guaimas <gguaimas85@gmail.com>
2023-04-23 21:37:04 -0300

    Crecion Component Spaceships

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 // Possible alternative: 'https://swapi.dev/api'
 //const baseURL = 'https://www.swapi.it/api';
 const baseURL = 'https://swapi.dev/api';
+const dataSpaceships = 'https://swapi.dev/api/starships';
 
 export const swGet = (url: string) =>
   axios.get(url, { baseURL }).then((res) => res.data);
